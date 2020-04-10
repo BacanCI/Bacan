78c287a184699783ccdc8fc47010748ffc1c5467
Rakesh Aravelly <raravelly@barnardos.org.au>
2020-04-10 17:12:37 +1000

    cyclic dependency validator  - wip

---
+++ src/Bakana.Core/Validators/StepsValidator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bakana.Core.Entities;
+using FluentValidation;
+
+namespace Bakana.Core.Validators
+{
+    public class StepsValidator : AbstractValidator<IList<Step>>
+    {
+        public StepsValidator()
+        {
+            RuleFor(steps => steps)
+                .Custom((steps, context) =>
+                {
+                    if (steps.All(s => s.Dependencies.Length > 0))
+                    {
+                        context.AddFailure("All the steps have dependencies. None can be executed");
+                    }
+                    //foreach (var step in steps)
+                    //{
+                    //    var dependentSteps = GetDependencies(step, steps.ToList());
+                    //    if (dependentSteps.Contains(step.Name))
+                    //    {
+                    //        context.AddFailure(nameof(Step.Dependencies), "Step has cyclic dependency");
+                    //    }
+                    //}
+                });
+        }
+
+        //private IList<string> GetDependencies(Step step, List<Step> steps)
+        //{
+        //    var dependencies = step.Dependencies;
+
+        //    if(dependencies == null || dependencies.Length == 0)
+        //        return new List<string>();
+
+        //    var dependentSteps = steps.Where(s => dependencies.Contains(s.Name));
+
+        //    var indirectDependencies = new List<string>();
+        //    foreach (var dependentStep in dependentSteps)
+        //    {
+        //        indirectDependencies.AddRange(GetDependencies(dependentStep, steps));
+        //    }
+
+        //    return indirectDependencies;
+        //}
+    }
+}
---
+++ src/Bakana.UnitTests/Validators/StepsValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Bakana.Core.Entities;
+using Bakana.Core.Validators;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Bakana.UnitTests.Validators
+{
+    [TestFixture]
+    public class StepsValidatorTests
+    {
+        private StepsValidator _stepsValidator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stepsValidator = new StepsValidator();
+        }
+
+        [Test]
+        public void Should_Have_Error_When_All_Steps_Have_Dependencies()
+        {
+            var steps = new List<Step>
+            {
+                new Step
+                {
+                    Name = "Step1",
+                    Dependencies = new[] {"Step2"}
+                },
+                new Step
+                {
+                    Name = "Step2",
+                    Dependencies = new []{"Step3"}
+                },
+                new Step
+                {
+                    Name = "Step3",
+                    Dependencies = new []{"Step1"}
+                }
+
+            };
+
+            var result = _stepsValidator.TestValidate(steps);
+
+            result.Errors.Should().Contain(e => e.ErrorMessage == "All the steps have dependencies. None can be executed");
+        }
+    }
+}

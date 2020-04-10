11144c37b31fd4ff306ef7ac21d757ea50a0f42c
Rakesh Aravelly <raravelly@barnardos.org.au>
2020-04-10 11:34:28 +1000

    Validators for batch and step entities - wip

---
+++ src/Bakana.UnitTests/Validators/BatchValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Bakana.Core.Entities;
+using Bakana.Core.Validators;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Bakana.UnitTests.Validators
+{
+    [TestFixture]
+    public class BatchValidatorTests
+    {
+        private BatchValidator _batchValidator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _batchValidator = new BatchValidator();
+        }
+
+        [Test]
+        public void Should_Have_Error_When_Id_Is_Null()
+        {
+            _batchValidator.ShouldHaveValidationErrorFor(s => s.Id, null as string)
+                .WithErrorMessage("Batch id is not specified");
+        }
+
+        [Test]
+        public void Should_Have_Error_When_No_Steps_Are_Supplied()
+        {
+            _batchValidator.ShouldHaveValidationErrorFor(s => s.Steps, new List<Step>())
+                .WithErrorMessage("A batch must have at least one step");
+        }
+    }
+}
---
+++ src/Bakana.UnitTests/Validators/StepValidatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Bakana.Core.Entities;
+using Bakana.Core.Validators;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Bakana.UnitTests.Validators
+{
+    [TestFixture]
+    public class StepValidatorTests
+    {
+        private StepValidator _stepValidator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var validatorContext = new StepValidatorContext
+            {
+                StepIds = new List<string>
+                {
+                    "Step1",
+                    "Step2"
+                }
+            };
+            _stepValidator = new StepValidator(validatorContext);
+        }
+
+        [Test]
+        public void Should_Have_Error_When_StepId_Is_Null()
+        {
+            _stepValidator.ShouldHaveValidationErrorFor(s => s.StepId, null as string)
+                .WithErrorMessage("Step id must be specified");
+        }
+
+        [Test]
+        public void Should_Have_Error_When_No_Commands_Are_Supplied()
+        {
+            var step = new Step();
+
+            var result = _stepValidator.TestValidate(step);
+
+            result.ShouldHaveValidationErrorFor(s => s.Commands)
+                .WithErrorMessage("A step must have at least one command");
+        }
+
+        [Test]
+        public void Should_Have_Error_When_Dependency_Is_Not_Valid()
+        {
+            var step = new Step
+            {
+                Dependencies = new []{"Step3"}
+            };
+
+            var result = _stepValidator.TestValidate(step);
+
+            result.ShouldHaveValidationErrorFor(s => s.Dependencies)
+                .WithErrorMessage("Unknown dependency :Step3");
+        }
+    }
+}

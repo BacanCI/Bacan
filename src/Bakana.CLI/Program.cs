be7f4564befcb88ce12645a7fdbc88c01fdc5c07
Neil Dobson <neil.dobson71@gmail.com>
2020-04-10 13:43:43 +1000

    Add Bakana CLI

---
+++ src/Bakana.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bakana.Client
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bakana!");
+        }
+    }
+}
